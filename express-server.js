3bf4cfcdeee9cdfac0c421b1f0d979a968868e1d
Armel Chesnais <armel.chesnais@gmail.com>
2017-05-09 20:50:07 +0000

    added new url page, POST handling, and random string generator

--- express-server.js
+++ express-server.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 var app = express();
 const PORT = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+/*** DATA ***/
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+/*** VIEWS/PROCESSING ***/
 
 // app.get("/", (req, res) => {
 //   res.end("Hello!");
@@ -22,6 +28,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
@@ -31,6 +41,11 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+});
+
 app.get("/hello", (req, res) => {
   res.end("<html><body> Hello <b>World</b></body></html>")
 });
@@ -38,3 +53,18 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`example app listening on port: ${PORT}!`);
 });
+
+
+/*** MISC FUNCTIONS ***/
+function generateRandomString() {
+   return genRandStringLength(6);
+}
+
+function genRandStringLength(size) {
+  let result = "";
+  choices = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-";
+  for (var i = 0; i < size; i++) {
+    result += choices.charAt(Math.random() * choices.length);
+  }
+  return result;
+}
---
+++ test.js
@@ -0,0 +1,11 @@
+function genRandStringLength(size) {
+  let result = "";
+  choices = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-";
+  for (var i = 0; i < size; i++) {
+    result += choices.charAt(Math.random() * choices.length);
+  }
+  return result;
+}
+
+console.log(genRandStringLength(6));
+
